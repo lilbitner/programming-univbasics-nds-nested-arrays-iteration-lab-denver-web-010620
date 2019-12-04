611dd699b8de88da2b3c6fe9ebf451ed6bdae7dd
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-04 22:25:27 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -17,10 +17,18 @@ end
 def find_greater_pair(src)
  outer_results = []
 row_index = 0
-while row_index < spice_rack.count do
+while row_index < array_2.count do
   element_index = 0
   inner_results = []
-  while element_index < spice_rack[row_index].count do
+  while element_index < array_2[row_index].count 
+  do
+    if array_2[row_index][element_index][0] > array_2[row_index][element_index][1] 
+      inner_results << spice_rack[row_index][element_index]
+    end
+    element_index += 1
+  end
+  outer_results << inner_results
+  row_index += 1
 end
 
 def total_even_pairs(src)
